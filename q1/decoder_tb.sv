d8ba0628cd95a6ff5741e3a17344700c72ece489
Xiaoxia Liu <xiaoxliu@kth.se>
2025-09-15 21:34:47 +0300

    q1

--- q1/decoder_tb.sv
+++ q1/decoder_tb.sv
@@ -11,4 +11,24 @@ module decoder_tb;
   );
 
   // Complete your testbench here
+  initial begin
+    for (int i = 0; i < 16; i++) begin
+      binary = i[3:0];
+      @(one_hot);  // Wait until the output changes
+
+      // Count how many 1 in one_hot
+      int cnt = 0;
+      for (int j = 0; j < 16; j++) begin
+        if (one_hot[j]) cnt++;
+      end
+
+      // Only one 1 and it at index i
+      if (cnt != 1 || !one_hot[i])
+        $display("FAIL %d", i);
+      else
+        $display("PASS %d", i);
+    end
+  end
+
+
 endmodule
